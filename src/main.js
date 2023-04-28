73319c54e8c5dcc822d1757d64a7f1b2213229a6
Vladyslav Molchan <Nksb2017@gmail.com>
2023-04-28 20:39:56 +0300

    openAI API

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import config from "config";
-import { ogg } from './ogg.js'
+import { ogg } from "./ogg.js";
+import { openai } from "./openai.js";
+import { code } from "telegraf/format";
 
 const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
 
 bot.on(message("voice"), async (ctx) => {
   try {
+    await ctx.reply(code("One moment please..."));
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
-    const userId = String(ctx.message.from.id)
-    const oggPath = await ogg.create(link.href, userId)
-    const mp3Path = await ogg.toMp3(oggPath, userId)
-    await ctx.reply(mp3Path);
+    const userId = String(ctx.message.from.id);
+    const oggPath = await ogg.create(link.href, userId);
+    const mp3Path = await ogg.toMp3(oggPath, userId);
+
+    const text = await openai.transcription(mp3Path);
+    await ctx.reply(code(`your request: ${text}`));
+
+    const messages = [{ role: openai.roles.USER, content: text }];
+    const response = await openai.chat(messages);
+
+    await ctx.reply(response.content);
   } catch (e) {
     console.log(`Error while voice message`, e.message);
   }
